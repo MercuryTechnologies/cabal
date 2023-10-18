a08129efcb0eb26ab81021a38883fc6f8c1636a9
Gabriella Gonzalez <GenuineGabriella@gmail.com>
2023-10-18 16:07:21 -0700

    Use response files for `ghc` invocations
    Before this change, `cabal` could fail with the following error message
    when building very large Haskell packages:
    
    ```
    ghc: createProcess: posix_spawnp: resource exhausted (Argument list too long)
    ```
    
    This is because when the number of modules or dependencies grows large
    enough, then the `ghc` command line can potentially exceed the
    `ARG_MAX` command line length limit.
    
    However, `ghc` supports response files in order to work around these
    sorts of command line length limitations, so this change enables the
    use of those response files.
    
    Note that this requires taking a special precaution to not pass RTS
    options to the response file because there's no way that `ghc` can
    support RTS options via the response file.  The reason why is because
    the Haskell runtime processes these options (not `ghc`), so if you
    store the RTS options in the response file then `ghc`'s command line
    parser won't know what to do with them.
    
    This means that `ghc` commands can still potentially fail if the RTS
    options get long enough, but this is less likely to occur in practice
    since RTS options tend to be significantly smaller than non-RTS
    options.

--- cabal-install/tests/UnitTests/Distribution/Client/ArbitraryInstances.hs
+++ cabal-install/tests/UnitTests/Distribution/Client/ArbitraryInstances.hs
@@ -215,7 +215,7 @@ instance Arbitrary ActiveRepoEntry where
 
 instance Arbitrary CombineStrategy where
     arbitrary = arbitraryBoundedEnum
-    shrink    = shrinkBoundedEnum
+    shrink    = UnitTests.Distribution.Client.ArbitraryInstances.shrinkBoundedEnum
 
 -------------------------------------------------------------------------------
 -- AllowNewer
